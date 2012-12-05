c172275ebdf81648d71a9297dc08c107fb16853f
Matt Clements <matt@mattclements.co.uk>
2012-12-05 08:25:56 +0000

    Changes

--- empowering_with_apis/js/custom.js
+++ empowering_with_apis/js/custom.js
@@ -1,1 +1,18 @@
-// write your script
+$(document).ready(function() {
+	
+	var fadeinBox = $("#scrollinglist1");
+	var fadeoutBox = $("#scrollinglist2");
+	
+	function fade() {
+		setTimeout(function(){
+			$(fadeinBox).fadeIn(500);
+		  	$(fadeoutBox).fadeOut(500, function () {
+		        var temp = fadeinBox;
+		        fadeinBox = fadeoutBox;
+		        fadeoutBox = temp;
+				setTimeout(fade, 2000);
+		    });
+		}, 2000);
+	}
+	fade();
+});
